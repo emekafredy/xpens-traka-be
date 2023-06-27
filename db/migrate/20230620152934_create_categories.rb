b2ece6a750e340786962674ae04a708d7e25f6f2
emekafredy <emeka.s.chinedu@gmail.com>
2023-06-27 13:33:43 +0100

    resolve migration issue on ci

--- db/migrate/20230620152934_create_categories.rb
+++ db/migrate/20230620152934_create_categories.rb
@@ -2,6 +2,7 @@
 
 class CreateCategories < ActiveRecord::Migration[7.0]
   def change
+    enable_extension 'pgcrypto' unless extension_enabled?('pgcrypto')
     create_table :categories, id: :uuid do |t|
       t.string :name
       t.string :section
